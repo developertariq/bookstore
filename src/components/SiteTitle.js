3cb8d027c62f72731b5e3dc43e02aa229daac527
developertariq <dev.tariqul.islam@gmail.com>
2023-01-20 11:07:42 +0600

    Add pagetitle component

---
+++ src/components/SiteTitle.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const SiteTitle = () => (
+  <h1
+    data-testid="site-title"
+    style={{
+      fontSize: '3rem',
+      fontWeight: '600',
+      marginBottom: '2rem',
+      lineHeight: '1em',
+      color: '#ececec',
+      textAlign: 'center',
+    }}
+  >
+    Bookstore CMS
+  </h1>
+);
+
+export default SiteTitle;
