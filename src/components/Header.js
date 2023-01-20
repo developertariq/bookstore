12baf98c9c884f39929151223623c09f956e963b
developertariq <dev.tariqul.islam@gmail.com>
2023-01-20 17:40:34 +0600

    Change markup header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,11 @@ import User from './User';
 
 const Header = () => {
   const headerStyle = {
-    padding: '10px 50px',
-    lineHeight: '1.5em',
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
-    backgroundColor: '#0a0a23',
-    color: '#fff',
-    borderBottom: '4px solid #fdb347',
+    backgroundColor: '#fff',
+    padding: '10px 48px',
   };
 
   return (
