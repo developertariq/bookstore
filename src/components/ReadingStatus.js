5e8869854dd69ebfceec96d61c98c419b7758bfc
developertariq <dev.tariqul.islam@gmail.com>
2023-01-20 17:39:31 +0600

    Add reading status component

---
+++ src/components/ReadingStatus.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ReadingStatus = (props) => {
+  const { id } = props;
+  return (
+    <div>
+      <p>Current Chapter</p>
+      <p>{id}</p>
+      <button type="button">Update Progress</button>
+    </div>
+  );
+};
+
+ReadingStatus.propTypes = {
+  id: PropTypes.string,
+};
+
+ReadingStatus.defaultProps = {
+  id: '',
+};
+
+export default ReadingStatus;
