29d764f41b23a4ec49745b13207a221934212de5
developertariq <dev.tariqul.islam@gmail.com>
2023-01-20 11:02:11 +0600

    Set route and link in index.js file

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Books from './pages/Books';
+import Categories from './pages/Categories';
+import Header from './components/Header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Books />} />
+        </Route>
+        <Route path="/Categories" element={<Categories />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
