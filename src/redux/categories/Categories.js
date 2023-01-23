84273af51411828ecfddafbbeec7a106cdda86e1
developertariq <dev.tariqul.islam@gmail.com>
2023-01-23 21:57:31 +0600

    Write actions and reducer for category

--- src/redux/categories/Categories.js
+++ src/redux/categories/Categories.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-const Categories = () => (
+const STATUS = 'STATUS';
+
+export function createWidget(category) {
+  return { type: STATUS, category };
+}
+
+const initialState = {
+  categories: [],
+};
+
+export default function categories(state = initialState, action) {
+  switch (action.type) {
+    case STATUS:
+      return { ...state, categories: action.categories };
+    default:
+      return state;
+  }
+}
+
+export const Categories = () => (
   <div className="container">
     <button type="button">Check status</button>
   </div>
 );
-
-export default Categories;
