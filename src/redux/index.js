deb2e7ed09d25553ab522cf393e3b35cc3923da7
developertariq <dev.tariqul.islam@gmail.com>
2023-01-26 02:13:47 +0600

    Add api reducer to root reducer

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import bookReducer from './book/books';
 import categoryReducer from './category/categories';
+import reducer from './book/fetchBooks';
 
 export default combineReducers({
   books: bookReducer,
   categories: categoryReducer,
+  api: reducer,
 });
